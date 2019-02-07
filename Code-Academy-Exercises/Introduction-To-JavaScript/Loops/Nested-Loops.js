8cff54e0842608d290686e37a0f69f30b8043764
mrnicholashunt <mrnicholashunt@gmail.com>
2019-02-06 19:06:23 -0700

    loops

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Nested-Loops.js
@@ -0,0 +1,14 @@
+// Write your code below
+
+let bobsFollowers = ['Bill', 'Ted', 'Tony', 'Steve'];
+let tinasFollowers = ['Bill', 'Ted', 'Kim'];
+let mutualFollowers = [];
+
+for (let i = 0; i < bobsFollowers.length; i++) {
+  for (let j = 0; j < tinasFollowers.length; j++) {
+    if (bobsFollowers[i] === tinasFollowers[j]) {
+      mutualFollowers.push(bobsFollowers[i]);
+    }
+  }
+};
+console.log(mutualFollowers)
---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Loops/Repeating-Tasks-Manually.js
@@ -0,0 +1,5 @@
+// Write your code below
+const vacationSpots = ['Thailand', 'Costa Rica', 'Panama']
+console.log(vacationSpots[0])
+console.log(vacationSpots[1])
+console.log(vacationSpots[2])
